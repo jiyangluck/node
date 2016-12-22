6880963569ce25deee86106fb9e76d97339cf9c5
lee <ljyteach@163.com>
2016-12-22 16:59:20 +0800

    x

--- jade/server.js
+++ jade/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
+var path = require("path");
 var app = express();
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","jade");
 
 app.get("/",function(req,res){
